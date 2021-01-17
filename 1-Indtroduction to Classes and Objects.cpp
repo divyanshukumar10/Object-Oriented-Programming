8f1bb3105df10f72b2fd8dc8ebf2798b6fa43a87
Divyanshu Kumar <38113387+divyanshukumar10@users.noreply.github.com>
2021-01-17 07:51:31 +0530

    Add files via upload

---
+++ 1-Indtroduction to Classes and Objects.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class HumanBeing{
+public :
+    void display(){
+        cout<<"hello aam a human being";
+    }
+
+};
+
+
+int main()
+{
+    HumanBeing divya;
+
+    divya.display();
+
+
+    return 0;
+}
