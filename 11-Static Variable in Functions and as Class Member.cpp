8a5c4846ee0ad4b6a745638f01ff49ba3fb1ce64
Divyanshu Kumar <38113387+divyanshukumar10@users.noreply.github.com>
2021-01-28 21:58:45 +0530

    Add files via upload

---
+++ 11-Static Variable in Functions and as Class Member.cpp
@@ -0,0 +1,35 @@
+#include <iostream> 
+using namespace std; 
+
+class Human
+{ 
+	public: static 
+	int human_count; 
+	Human()
+	{ 
+		human_count++;
+	} 
+
+	void humanTotal()
+	{ 
+		cout << "There are "<<human_count<<" peoples are in this program" <<endl;
+	} 
+
+	static void humanCount()
+	{ 
+		cout << "There are "<<human_count<<" peoples are in this program " <<endl; 
+	} 
+}; 
+
+int Human::human_count = 0; 
+
+int main() 
+{ 
+	cout << Human::human_count<<endl; 
+	Human divya; 
+	Human anshu; 
+	Human kumar; 
+	Human abhi; 
+	Human ::humanCount();
+	cout << Human::human_count<<endl; 
+}
