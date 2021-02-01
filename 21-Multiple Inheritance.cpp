91ac019f57c9dfeb0abe3b51c91258bae9a6942c
Divyanshu Kumar <38113387+divyanshukumar10@users.noreply.github.com>
2021-02-01 21:54:29 +0530

    Add files via upload

---
+++ 21-Multiple Inheritance.cpp
@@ -0,0 +1,51 @@
+#include <iostream> 
+#include <string> 
+using namespace std; 
+
+class Father
+{ 
+	public: 
+		int height; 
+
+		void askFather()
+		{ 
+			cout <<"am your father ask me what u want"<<endl;
+		}
+}; 
+
+class Mother
+{ 
+	public: 
+		string skincolor; 
+		void askMother()
+		{ 
+			cout <<"am your mother ask me what u want"<<endl; 
+		} 
+}; 
+
+class Child : public Father, public Mother
+{ 
+	public: void askParents()
+	{ 
+		cout <<"am asking my parents"<<endl; 
+	} 
+
+	void setColorandHeight(string icolor,int iheight)
+	{ 
+		skincolor = icolor; height = iheight; 
+	} 
+
+	void display()
+	{ 
+		cout <<"height is "<<height<<" and color is "<<skincolor<<endl;
+	} 
+}; 
+
+int main() 
+{ 
+	Child divyanshu; 
+	divyanshu.setColorandHeight("white",6); 
+	divyanshu.display(); 
+	divyanshu.askFather(); 
+	divyanshu.askMother();
+}
