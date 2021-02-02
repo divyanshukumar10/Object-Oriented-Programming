9e60f8ba6458b35e97b3eeba49cef772f07649ac
Divyanshu Kumar <38113387+divyanshukumar10@users.noreply.github.com>
2021-02-02 21:54:58 +0530

    Add files via upload

---
+++ 27-Polymorphism and Virtual Methods.cpp
@@ -0,0 +1,40 @@
+#include <iostream> 
+using namespace std; 
+
+class Person
+{ 
+	public: virtual 
+	void introduce()
+	{ 
+		cout <<"hey from person"<<endl; 
+	} 
+}; 
+
+class Student : public Person
+{ 
+	public: void introduce()
+	{ 
+		cout <<"hey from student"<<endl; 
+	} 
+}; 
+
+class Farmer : public Person
+{ 
+	public: void introduce()
+	{ 
+		cout <<"hey from Farmer"<<endl; 
+	} 
+}; 
+
+void whosThis(Person &p)
+{
+	p.introduce(); 
+} 
+
+int main() 
+{ 
+	Farmer divya; 
+	Student anshu; 
+	whosThis(divya); 
+	whosThis(anshu); 
+}
